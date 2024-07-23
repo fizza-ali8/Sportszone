0b5276451fd2edb13d8b225ba120b421863c2612
Alizayan <142315412+alibahai@users.noreply.github.com>
2024-07-23 15:13:29 +0500

    h1 tag removed

--- src/pages/News.js
+++ src/pages/News.js
@@ -6,7 +6,6 @@ import '../Style/News.css';
 const News = () => {
   return (
     <>
-      <h1>News</h1>
       <div className="container">
         {cardData.map((card, index) => (
           <Card key={index} card={card} />
