9c68d01a9f96e487b769453988b28ab96bb8b24b
Alizayan <142315412+alibahai@users.noreply.github.com>
2024-07-29 14:58:26 +0500

    UPDATED CODE HOME CREATED

--- src/Helper/HomeCardData.js
+++ src/Helper/HomeCardData.js
@@ -1,22 +1,81 @@
-
+// src/Helper/HomeCardData.js
 
 const HomeCardData = [
     {
-        id: 1,
-        text1: "Some text here",
-        country1: { name: "PAK", goals: 1, flag: "path/to/pak-flag.png" },
-        country2: { name: "Jpn", goals: 4, flag: "path/to/jpn-flag.png" },
-        text2: "More text here",
+      id: 1,
+      text1: "Final - 30th Azlan Shah Hockey Cup",
+      country1: { name: "PAK", goals: 1, flag: "FLAGS_IMAGES/PAKISTAN.JPEG" },
+      country2: { name: "Jpn", goals: 4, flag: "FLAGS_IMAGES/JAPAN.JPEG" },
+      text2: "Japan won by penalty shootouts",
     },
-    // Add 11 more similar objects with unique id
-    // Example:
-    {
-        id: 2,
-        text1: "Some other text",
-        country1: { name: "USA", goals: 2, flag: "path/to/usa-flag.png" },
-        country2: { name: "Ger", goals: 3, flag: "path/to/ger-flag.png" },
-        text2: "Additional text",
-    },
-];
-
-export default HomeCardData;
+      {
+      id: 2,
+      text1: "3rd Match - Pakistan Premier League",
+      country1: { name: "PAK ARMY", goals: 2, flag: "FLAGS_IMAGES/ARMY.JPEG" },
+      country2: { name: "WAPDA", goals: 1, flag: "FLAGS_IMAGES/WAPADA.JPEG" },
+      text2: "Pakistan Army won",
+    },  {
+        id: 3,
+        text1: "3rd T201 - Pakistann tour of Zimbabwe",
+        country1: { name: "PAK", goals: "182-4(20)" , flag: "FLAGS_IMAGES/PAKISTAN.JPEG" },
+        country2: { name: "ZIM", goals: "159-6", flag: "FLAGS_IMAGES/ZIMBAVE.JPEG" },
+        text2: "Zim needs 24 of 12 balls",
+      },  {
+        id: 4,
+        text1: "Final - World Championship of Legends",
+        country1: { name: "PAK", goals: 0, flag: "FLAGS_IMAGES/PAKISTAN.JPEG" },
+        country2: { name: "TBA", goals: 0, flag: "FLAGS_IMAGES/BLACK.JPEG" },
+        text2: "Saturday, 13 Jul, 9:00 PM",
+      },  {
+        id: 5,
+        text1: "3rd Match - Pakistan Premier League",
+        country1: { name: "PAK ARMY", goals: 2, flag: "FLAGS_IMAGES/ARMY.JPEG" },
+        country2: { name: "WAPADA", goals: 1, flag: "FLAGS_IMAGES/WAPADA.JPEG" },
+        text2: "Pakistan Army won",
+      },  {
+        id: 6,
+        text1: "Final - World Championship of Legends",
+        country1: { name: "PAK", goals: 0, flag: "FLAGS_IMAGES/PAKISTAN.JPEG" },
+        country2: { name: "TBA", goals: 0, flag: "FLAGS_IMAGES/BLACK.JPEG" },
+        text2: "Saturday, 13 Jul, 9:00 PM",
+      },  {
+        id: 7,
+        text1: "3rd T201 - India tour of Zimbabwe, 2024",
+        country1: { name: "IND", goals: "182-4(20)", flag: "FLAGS_IMAGES/INDIAN.JPEG" },
+        country2: { name: "ZIM", goals: "159-6", flag: "FLAGS_IMAGES/ZIMBAVE.JPEG" },
+        text2: "India won by 23 runs",
+      },  {
+        id: 8,
+        text1: "2nd Test - Richards-Botham Trophy, 2024",
+        country1: { name: "ENGLAND", goals: 0, flag: "FLAGS_IMAGES/ENGLAND.JPEG" },
+        country2: { name: "WESTINDIES", goals: 0, flag: "FLAGS_IMAGES/WESTINDIES.JPEG" },
+        text2: "Tomorrow, 3:30pm",
+      },  {
+        id: 9,
+        text1: "3rd Match - Pakistan Premier League",
+        country1: { name: "PAK ARMY", goals: 2, flag: "FLAGS_IMAGES/ARMY.JPEG" },
+        country2: { name: "WAPADA", goals: 1, flag: "FLAGS_IMAGES/WAPADA.JPEG" },
+        text2: "Pakistan Army won",
+      },  {
+        id: 10,
+        text1: "Final - World Championship of Legends",
+        country1: { name: "PAK", goals: 0, flag: "FLAGS_IMAGES/PAKISTAN.JPEG" },
+        country2: { name: "TBA", goals: 0, flag: "FLAGS_IMAGES/BLACK.JPEG" },
+        text2: "Saturday, 13 Jul, 9:00 PM",
+      },  {
+        id: 11,
+        text1: "3rd T201 - India tour of Zimbabwe, 2024",
+        country1: { name: "IND", goals: "182-4(20)", flag: "FLAGS_IMAGES/INDIAN.JPEG" },
+        country2: { name: "ZIM", goals: "159-6", flag: "FLAGS_IMAGES/ZIMBAVE.JPEG" },
+        text2: "India won by 23 runs",
+      },  {
+        id: 12,
+        text1: "2nd Test - Richards-Botham Trophy, 2024",
+        country1: { name: "ENGLAND", goals: 0, flag: "FLAGS_IMAGES/ENGLAND.JPEG" },
+        country2: { name: "WESTINDIES", goals: 0, flag: "FLAGS_IMAGES/WESTINDIES.JPEG" },
+        text2: "Tomorrow, 3:30pm",
+      },
+  ];
+  
+  export default HomeCardData;
+  
